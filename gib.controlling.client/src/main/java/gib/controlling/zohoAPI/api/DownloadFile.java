be60c57372712dea97cf3c05e95b639241ce9666
peter.parys <parys@uniquemail.net>
2017-11-13 18:03:09 +0100

    debug log for api downloads

--- gib.controlling.client/src/main/java/gib/controlling/zohoAPI/api/DownloadFile.java
+++ gib.controlling.client/src/main/java/gib/controlling/zohoAPI/api/DownloadFile.java
@@ -10,13 +10,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.Logger;
 
 public class DownloadFile extends ZohoDocsUtils {
 
 	private static final String FILES_URL = "https://apidocs.zoho.eu/files/v1/content/";
 
+	private Logger log;
+
 	public DownloadFile(String authToken) {
 		this.authToken = authToken;
+		log = Logger.getLogger(DownloadFile.class.getName());
 	}
 
 	public byte[] download(Path path) throws FileNotFoundException {
@@ -35,7 +39,9 @@ public class DownloadFile extends ZohoDocsUtils {
 			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 			HttpResponse response = httpClient.execute(docsGet);
 			InputStream is = response.getEntity().getContent();
-			return IOUtils.toByteArray(is);
+			byte[] responseByteArray = IOUtils.toByteArray(is);
+			log.debug(new String(responseByteArray));
+			return responseByteArray;
 		} catch (Exception ee) {
 		}
 		return null;
